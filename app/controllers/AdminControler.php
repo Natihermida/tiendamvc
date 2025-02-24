2d40cebf65b31f58d4199e6d1312825498677031
NatiHermida <hermidaltamarnati@gmail.com>
2025-02-24 20:30:40 +0100

    tiendamvc

---
+++ app/controllers/AdminControler.php
@@ -0,0 +1,14 @@
+<?php
+namespace Formacom\controllers;
+use Formacom\Core\Controller;
+class AdminController extends Controller{
+    public function index(...$params){
+        $data = ['mensaje' => '¡Bienvenido a la página de inicio!'];
+        $this->view('home', $data);
+    }
+    public function new(){
+        echo "Hola desde New de HomeController";
+    }
+    
+}
+?>
